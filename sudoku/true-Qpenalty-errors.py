f4ac4c33525fd51c494142a836f857e4a6f0ddfd
Brandon Amos <bamos@cs.cmu.edu>
2017-03-27 20:33:20 -0400

    true-Qpenalty-errors: Add MSE.

--- sudoku/true-Qpenalty-errors.py
+++ sudoku/true-Qpenalty-errors.py
@@ -10,15 +10,16 @@ from train import computeErr
 
 for boardSz in (2,3):
     print('# Board Sz: {}'.format(boardSz))
-    print('| {:15s} | {:15s} |'.format('Qpenalty', 'Error'))
+    print('| {:15s} | {:15s} | {:15s} |'.format('Qpenalty', '% Boards Wrong', 'MSE'))
     with open('data/{}/features.pt'.format(boardSz), 'rb') as f:
-        unsolvedBoards = Variable(torch.load(f).cuda()[:5,:,:,:])
+        unsolvedBoards = Variable(torch.load(f).cuda()[:,:,:,:])
         nBoards = unsolvedBoards.size(0)
+    with open('data/{}/labels.pt'.format(boardSz), 'rb') as f:
+        solvedBoards = Variable(torch.load(f).cuda()[:nBoards,:,:,:])
     for Qpenalty in np.logspace(-3, 1, num=10, base=10.):
-        try:
-            model = models.OptNet(boardSz, Qpenalty, trueInit=True).cuda()
-            err = computeErr(model(unsolvedBoards).data)/nBoards
-            print('| {:15f} | {:15f} |'.format(Qpenalty, err))
-        except:
-            continue
+        model = models.OptNetEq(boardSz, Qpenalty, trueInit=True).cuda()
+        preds = model(unsolvedBoards).data
+        err = computeErr(preds)/nBoards
+        mse = (preds - solvedBoards.data).mean()
+        print('| {:15f} | {:15f} | {:15f} |'.format(Qpenalty, err, mse))
     print('\n\n')
