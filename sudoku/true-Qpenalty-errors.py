70abd84d852dfd4a03ee2aca7274c38195afad6c
Brandon Amos <bamos@cs.cmu.edu>
2017-03-27 20:33:58 -0400

    true-Qpenalty-errors: Add note about MSE.

--- sudoku/true-Qpenalty-errors.py
+++ sudoku/true-Qpenalty-errors.py
@@ -20,6 +20,7 @@ for boardSz in (2,3):
         model = models.OptNetEq(boardSz, Qpenalty, trueInit=True).cuda()
         preds = model(unsolvedBoards).data
         err = computeErr(preds)/nBoards
+        # MSE is not an exact metric because a board might have multiple solutions.
         mse = (preds - solvedBoards.data).mean()
         print('| {:15f} | {:15f} | {:15f} |'.format(Qpenalty, err, mse))
     print('\n\n')
